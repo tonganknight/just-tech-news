70e615da5b5b66c47a2cdd6169a4832bbe072c53
Malachi <malachialusa@gmail.com>
2021-02-10 10:04:37 -0700

    I added api end points

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -47,11 +47,41 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+  //query operation
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    //res.json({ user: dbUserData });
+
+    // Verify user
+    const validPassword = dbUserData.checkPassword(req.body.password)
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+    
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+
+  });  
+});
+
 router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
   // pass in req.body instead to only update what's passed through
   User.update(req.body, {
+    //option to add in hooks 
+    individualHooks: true,
     where: {
       id: req.params.id
     }
